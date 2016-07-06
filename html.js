1c168c75968512daad5a91df9511aced342729ba
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-07-07 01:56:30 +0300

    add favicons

--- html.js
+++ html.js
@@ -21,18 +21,20 @@ module.exports = React.createClass({
         return (
             <html lang="en">
             <head>
-              <meta charSet="utf-8" />
-              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
-              <title>
-                { title }
-              </title>
-              { font }
-              { css }
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+                <title>
+                    { title }
+                </title>
+                { font }
+                { css }
+                <link rel="shortcut icon" href={ prefixLink('/favicon-32.png') } />
+                <link rel="apple-touch-icon" href={ prefixLink('/favicon-144.png') } />
             </head>
             <body>
-              <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
-              <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
+                <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
+                <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
             </body>
             </html>
         )
