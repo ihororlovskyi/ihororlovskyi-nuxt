eb4d7b4835f4ae6a005acffda5e5775cdab8f184
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-12-06 11:03:01 +0200

    upd SiteNav

--- components/SiteNav/index.jsx
+++ components/SiteNav/index.jsx
@@ -11,19 +11,23 @@ class SiteNav extends React.Component {
             <nav className='blog-nav'>
                 <ul>
                     <li>
-                        <Link to={ prefixLink('/experience')} className={ location.pathname === prefixLink('/experience') ? "current" : null }> Experience
+                        <Link to={ prefixLink('/')} className={ location.pathname === prefixLink('/') ? "current" : null }>
+                            Projects
                         </Link>
                     </li>
                     <li>
-                        <Link to={ prefixLink('/about')} className={ location.pathname === prefixLink('/about') ? "current" : null }> About me
+                        <Link to={ prefixLink('/experience/')} className={ location.pathname === prefixLink('/experience') ? "current" : null }>
+                            Experience
                         </Link>
                     </li>
                     <li>
-                        <Link to={ prefixLink('/')} className={ location.pathname === prefixLink('/') ? "current" : null }> Projects
+                        <Link to={ prefixLink('/about/')} className={ location.pathname === prefixLink('/about') ? "current" : null }>
+                            About me
                         </Link>
                     </li>
                     <li>
-                        <Link to={ prefixLink('/contact')} className={ location.pathname === prefixLink('/contact') ? "current" : null }> Contact me
+                        <Link to={ prefixLink('/contact/')} className={ location.pathname === prefixLink('/contact') ? "current" : null }>
+                            Contact me
                         </Link>
                     </li>
                 </ul>
