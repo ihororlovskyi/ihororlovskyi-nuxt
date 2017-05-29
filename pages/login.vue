fc60419d3a714cfcb1362218568981d4a77d4e9c
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-05-29 21:16:17 +0300

    add login.vue

---
+++ pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='login'>
+    <form class='login__form' action=''>
+      <div class='login__form-title'>Login</div>
+      <input class='login__form-input' type='email' placeholder='email'>
+      <input class='login__form-input' type='password' placeholder='password'>
+      <input class='login__form-submit' type='submit' value='Login'>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from '~plugins/axios'
+
+export default {
+  head: {
+    title: 'login',
+    meta: [
+      { name: 'description', content: 'login page description' }
+    ]
+  }
+}
+</script>
+
+<style lang="scss">
+.login {
+
+  &__form {
+    max-width: 300px;
+    margin: 0 auto;
+
+    &-title {
+    }
+
+    &-input {
+      width: 100%;
+    }
+
+    &-submit {
+    }
+  }
+}
+</style>
