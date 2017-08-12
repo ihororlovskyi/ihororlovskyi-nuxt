811e9b496b255a3812f5ef991892e633f0ed2f1a
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-08-13 01:04:36 +0300

    upd pages/portfolio.vue

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -3,7 +3,10 @@
     <h1>Portfolio</h1>
     <nav class="portfolio__list">
       <div class="portfolio__item" v-for="(site, key) in sites">
-        <nuxt-link class="portfolio__link" :to="{ path: `/site/${key}`}">{{ site.title }}</nuxt-link>
+        <nuxt-link class="portfolio__link" :to="{ path: `/site/${key}`}">
+          <div>{{ site.title }}</div>
+          <div v-if='site.date'>{{ site.date }}</div>
+        </nuxt-link>
       </div>
     </nav>
     <p>and many more . . .</p>
