a1f119387ce6d6e7a7d763ea01073902326d25a5
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-08-27 16:08:56 +0300

    rename: chess -> rooster-chess

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ module.exports = {
       '/site/mcriviera',
       '/site/skoryk',
       '/site/vatmanman',
-      '/site/chess',
+      '/site/rooster-chess',
       '/site/sentimony',
       '/site/promorepublic',
       '/site/promorepublic-blog',
